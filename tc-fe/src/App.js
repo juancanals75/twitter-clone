a8107ac3b9816061fd867e1d67565dc21aceac41
Juan Canals <juancanals75@gmail.com>
2021-03-18 21:16:31 -0300

    Initial setup without docker

---
+++ tc-fe/src/App.js
@@ -0,0 +1,15 @@
+import doge from './assets/doge.png'
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={doge} className='App-logo' alt='logo' />
+				<p>This is going to be a twitter clone</p>
+			</header>
+		</div>
+	)
+}
+
+export default App
