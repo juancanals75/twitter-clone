fc02683b9e4002d78637b4a709a8a20e11a2980d
Juan Canals <juancanals75@gmail.com>
2021-04-16 17:23:19 -0300

    showing a table of tweets

--- tc-fe/src/App.js
+++ tc-fe/src/App.js
@@ -1,41 +1,46 @@
 import doge from './assets/doge.png'
 import './App.css'
 import DummyForm from './DummyForm'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
 	// 'http://localhost:5000/posts'
+	const [tweetsTable, setTweetsTable] = useState([])
+
+	async function getTweets() {
+		try {
+			const { data } = await axios.get('http://localhost:5000/posts')
+			console.log(data)
+			setTweetsTable(data.messages)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	useEffect(() => getTweets(), [])
 
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<h1>DOGER</h1>
 				<img src={doge} className='App-logo' alt='logo' />
+				<button onClick={() => getTweets()}>REFRESH</button>
 				<table style={{ width: '100%' }}>
 					<tr>
-						<th>Firstname</th>
-						<th>Lastname</th>
-						<th>Age</th>
+						<th>Title</th>
+						<th>Message</th>
 					</tr>
-					<tr>
-						<td>Jill</td>
-						<td>Smith</td>
-						<td>50</td>
-					</tr>
-					<tr>
-						<td>Eve</td>
-						<td>Jackson</td>
-						<td>94</td>
-					</tr>
+					{tweetsTable &&
+						tweetsTable.map(tweet => (
+							<tr>
+								<td>{tweet.title}</td>
+								<td>{tweet.message}</td>
+							</tr>
+						))}
 				</table>
 				<h5>TRY MAKING A POST</h5>
 				<div>
-					{/* <form onSubmit={dummySubmitFunction}>
-						<label for='title'>With a title</label>
-						<input type='text' name='title' id='title' />
-						<label for='message'>And a message</label>
-						<input type='text' name='message' id='message' />
-						<button type='submit'>SEND IT!</button>
-					</form> */}
 					<DummyForm />
 				</div>
 			</header>
