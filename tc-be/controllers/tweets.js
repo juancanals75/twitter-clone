c8f518ea8e4b9c82b024e44ba3e1366abe326324
Juan Canals <juancanals75@gmail.com>
2021-06-14 20:09:27 -0300

    Added paginated request for posts

--- tc-be/controllers/tweets.js
+++ tc-be/controllers/tweets.js
@@ -1,12 +1,23 @@
 import Tweet from '../models/tweet.js'
 
 export const getTweets = async (req, res) => {
-	try {
-		const tweetsArray = await Tweet.find()
+	const page = parseInt(req.query.page) || 1
+	const limit = parseInt(req.query.limit) || 10
+	const skipIndex = (page - 1) * limit
 
+	try {
+		const totalTweets = await Tweet.countDocuments()
+		const totalPages = Math.ceil(totalTweets / limit)
+
+		if (page > totalPages) throw new Error("Page doesn't exist")
+
+		const posts = await Tweet.find().limit(limit).skip(skipIndex).exec()
 		res.status(200).json({
-			total_messages: tweetsArray.length,
-			messages: tweetsArray,
+			total_tweets: totalTweets,
+			current_page: page,
+			next_page: totalPages > page ? page + 1 : null,
+			page_size: limit,
+			results: posts,
 		})
 	} catch (error) {
 		res.status(404).json({ message: error.message })
@@ -17,8 +28,12 @@ export const createTweet = async (req, res) => {
 	const tweet = new Tweet(req.body)
 
 	try {
-		const newTweet = await tweet.save()
-		res.status(201).json(newTweet)
+		if (tweet.message.length > 200) {
+			throw new Error("Post can't be longer than 200 characters")
+		}
+
+		await tweet.save()
+		res.status(201).json({ message: 'message posted succesfully' })
 	} catch (error) {
 		res.status(404).json({ message: error.message })
 	}
--- tc-be/models/tweet.js
+++ tc-be/models/tweet.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose'
 const tweetSchema = mongoose.Schema({
 	message: String,
 	created_at: { type: Date, default: Date.now },
-	user_id: String,
+	username: String,
 	coin_side: String,
-	reply_count: Number,
-	favorite_count: Number,
-	media_id: Number,
+	// reply_count: Number,
+	// favorite_count: Number,
+	// media_id: Number,
 })
 
 const Tweet = mongoose.model('Tweet', tweetSchema)
