2484f86b8eb8e73eddc46ae787b9911e33aa340d
Juan Canals <juancanals75@gmail.com>
2021-06-28 20:49:03 -0300

    user validation when positing and get posts from each side

--- tc-be/controllers/tweets.js
+++ tc-be/controllers/tweets.js
@@ -1,17 +1,26 @@
 import Tweet from '../models/tweet.js'
+import User from '../models/user.js'
 
 export const getTweets = async (req, res) => {
 	const page = parseInt(req.query.page) || 1
 	const limit = parseInt(req.query.limit) || 10
+	const coinSide = req.query.side
 	const skipIndex = (page - 1) * limit
 
 	try {
 		const totalTweets = await Tweet.countDocuments()
 		const totalPages = Math.ceil(totalTweets / limit)
 
+		if (!coinSide) {
+			throw new Error('You must specify a coin side')
+		}
+
 		if (page > totalPages) throw new Error("Page doesn't exist")
 
-		const posts = await Tweet.find().limit(limit).skip(skipIndex).exec()
+		const posts = await Tweet.find({ coin_side: coinSide })
+			.limit(limit)
+			.skip(skipIndex)
+			.exec()
 		res.status(200).json({
 			total_tweets: totalTweets,
 			current_page: page,
@@ -30,6 +39,22 @@ export const createTweet = async (req, res) => {
 	try {
 		if (tweet.message.length > 200) {
 			throw new Error("Post can't be longer than 200 characters")
+		}
+
+		if (!tweet.message || !tweet.username || !tweet.coin_side) {
+			throw new Error('Missing information')
+		}
+
+		if (tweet.coin_side !== 'dark' && tweet.coin_side !== 'light') {
+			throw new Error("Coin side can only be 'light' or 'dark'")
+		}
+
+		const usernameValidation = await User.findOne({
+			username: tweet.username,
+		})
+
+		if (!usernameValidation) {
+			throw new Error('Username does not exist')
 		}
 
 		await tweet.save()
